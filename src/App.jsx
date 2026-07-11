abea62cb4b7683e2c14d12cba7cd86995da1b608
abdelhafid37 <abdelhafidelhouari776@gmail.com>
2026-07-11 18:29:35 +0100

    chore: project scaffold & initial setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center w-full min-h-dvh">
+      <h1 className="font-mono text-7xl">Minh Pham Portfolio Remake.</h1>
+    </div>
+  );
+}
+
+export default App;
